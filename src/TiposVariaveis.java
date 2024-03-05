c66f7c231e3b4943eb0564312c536a5f93cfe9a4
Thiago Moisés Braga Montalvão <111362865+Thiago-Moises@users.noreply.github.com>
2024-03-05 20:56:23 -0300

    Add files via upload

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    @SuppressWarnings("unused")
+    public static void main(String[] args) throws Exception {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        int numero = 1;
+
+        System.out.print(numero);
+
+    }
+}
